a2d8c220a77e49dd9b0a8eb7faf8ad8e8b834f7c
Zitas45 <silasjunicke@googlemail.com>
2025-11-24 13:06:30 +0100

    fix use createwebhashhistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import Login from '../views/Login.vue';
 import Admin from '../views/Admin.vue';
@@ -25,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-   history: createWebHistory('/unikinoberlin/'),
+   history: createWebHashHistory(),
   routes,
 });
 
